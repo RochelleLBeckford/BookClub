84e2dc2c8013644c88c16d0cfa4dee7d7ad806c0
RochelleLBeckford <r.beckford@me.com>
2023-02-24 15:52:16 -0500

    update code

--- src/main/java/com/rochelle/book_club/repositories/UserRepository.java
+++ src/main/java/com/rochelle/book_club/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.rochelle.book_club.repositories;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+// import org.springframework.stereotype.Repository;
 
 import com.rochelle.book_club.models.User;
 
